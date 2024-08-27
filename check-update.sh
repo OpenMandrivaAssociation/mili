309820fc3a681f87748f569f045f6fc2aa12945b
Bernhard Rosenkränzer <bero@lindev.ch>
2024-08-27 17:14:41 +0200

    Don't let the abandoned project break auto-update

---
+++ check-update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# The project is abandoned upstream, there will not be any new releases
+# (but since the repository has been deleted, auto-update will time out
+# prompting for a password because gitlab thinks it might be private)
+echo 3.1.0
